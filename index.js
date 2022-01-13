2f4796c4847f6072ea2161dcdc71c6a8ddea9ea4
Joshua Patton <joshuapattonweb@gmail.com>
2022-01-13 12:54:55 -0500

    wip

---
+++ index.js
@@ -0,0 +1,22 @@
+const cohort1 = process.argv[2]
+const userName = process.env.USERNAME
+
+if (cohort1 === 'web-49') {
+    console.log(`${cohort1} is the best forever!`)
+} else {
+    console.log(`booooh! cohort ${cohort1}`)
+}
+
+console.log(`the user is ${userName}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.json({ message: 'hey there' })
+})
+
+const port = 9000
+app.listen(port, () => {
+    console.log(`Listening on port, ${port}`)
+})
